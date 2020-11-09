6da3a774c9a60a10f86301190c58e72a63bb0600
paminus-kingori <pkmurungi.pm@gmail.com>
2020-11-10 00:48:36 +0300

    initial commit

---
+++ action.js
@@ -0,0 +1,39 @@
+body = document.querySelector(".body");
+toggleBtn = document.querySelector(".toggle-btn");
+circle = document.querySelector(".white-circle");
+cards = document.querySelectorAll(".media-card");
+smallCards = document.querySelectorAll(".view-card");
+viewNumber = document.querySelectorAll(".view-number");
+largeNum = document.querySelectorAll(".large-number");
+title = document.querySelector(".media-title");
+overview = document.querySelector(".overview");
+attr = document.querySelector(".attribution");
+
+let textArray = [viewNumber,largeNum];
+let textArray2 = [title,overview,attr];
+
+
+
+toggleBtn.addEventListener('click',()=>{
+    body.classList.toggle("dark");
+    toggleBtn.classList.toggle("green");
+    circle.classList.toggle("move");
+    circle.classList.toggle("dark");
+    smallCards.forEach(cards => {        
+        cards.classList.toggle("card-dark");
+    });
+    cards.forEach(cards => {       
+        cards.classList.toggle("card-dark");
+    });
+    textArray.forEach(textArray =>{
+        textArray.forEach(textArray =>{
+            textArray.classList.toggle("white");
+        });
+    });
+
+    textArray2.forEach(textArray2 =>{
+        textArray2.classList.toggle("white");
+    });
+});
+
+
