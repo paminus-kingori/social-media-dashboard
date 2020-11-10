147711172923091fd03f0ac09060bfb9397b91ef
paminus-kingori <pkmurungi.pm@gmail.com>
2020-11-10 20:47:39 +0300

    added bright grey

--- action.js
+++ action.js
@@ -9,6 +9,8 @@ title = document.querySelector(".media-title");
 overview = document.querySelector(".overview");
 attr = document.querySelector(".attribution");
 topBg=document.querySelector(".lighterBg");
+viewText = document.querySelectorAll(".view-text");
+followers = document.querySelectorAll(".followers");
 
 let textArray = [viewNumber,largeNum];
 let textArray2 = [title,overview,attr];
@@ -16,19 +18,29 @@ let textArray2 = [title,overview,attr];
 
 
 toggleBtn.addEventListener('click',()=>{
+
     body.classList.toggle("dark");
     toggleBtn.classList.toggle("green");
     circle.classList.toggle("move");
     circle.classList.toggle("dark");
     topBg.classList.toggle("topBg");
+
+    viewText.forEach(viewText => {
+    viewText.classList.toggle("brightGrey");
+    });
+
+    followers.forEach(followers => {
+        followers.classList.toggle("brightGrey");
+            });
+
     smallCards.forEach(cards => {        
         cards.classList.toggle("card-dark");
     });
     cards.forEach(cards => {       
         cards.classList.toggle("card-dark");
     });
-    textArray.forEach(textArray =>{
-        textArray.forEach(textArray =>{
+    textArray.forEach(textArray => {
+        textArray.forEach(textArray => {
             textArray.classList.toggle("white");
         });
     });
